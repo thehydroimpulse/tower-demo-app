e2db43c23190dac2f8647d9aa0b491e3f604dc08
Daniel Fagnan <dnfagnan@gmail.com>
2013-09-18 15:56:08 -0600

    update
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var tower = require('tower'),
+    view = tower.view,
     server = tower.server,
     app = server(),
     get = server.get,
     post = server.post,
     put = server.put;
 
+// Register the view folder.
+// This will start watching the files.
+view.add(__dirname + '/views');
+
+/**
+ * GET /
+ * @route index
+ * @render index
+ */
+
 get('index', '/')
 .action(function*(){
-  this.body = 'Hello World';
+  this.scope.title = 'Hello World';
+  this.render('index', 'home');
 });
 
 app.listen();
