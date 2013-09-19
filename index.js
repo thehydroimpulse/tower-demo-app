daf0205773bfc500c6b5293691e46af00103d3b6
Daniel Fagnan <dnfagnan@gmail.com>
2013-09-19 16:54:12 -0600

    working view rendering
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ var tower = require('tower'),
 
 // Register the view folder.
 // This will start watching the files.
-view.add(__dirname + '/views');
+view.add('template', __dirname + '/views/templates');
+view.add('layout', __dirname + '/views/layouts');
+
 
 /**
  * GET /
@@ -35,63 +37,6 @@ get('index', '/')
   this.render('index', 'home');
 });
 
-/**
- * GET /real
- *
- * Real-time demo.
- *
- * @route real
- * @render index
- * @layout home
- * @public
- */
-
-get('index', '/real')
-.action(function *(){
-  // Add a new scope property:
-  this.scope.title = 'Hello World';
-
-  // Listen on a websocket message:
-  // This endpoint will only be active as long as
-  // the route is active. If you want a long-term
-  // subscription, you'll need to place it outside
-  // any specific routes.
-  this.subscribe('connect', function *(){
-    // Access the `message` property.
-    console.log(this.message);
-
-    // Emit a new message to __all__ users connected:
-    // `on` and `emit` are used to direct messages
-    // from server -> server, or server->clients (read:
-    // multiple clients).
-    //
-    // `subscribe` and `publish` are used for hooking
-    // to individual users (i.e the currently connected user.)
-    server.emit('new user', {
-      id: this.message.id,
-      username: this.message.username
-    });
-  });
-
-  // Render the `real` view, under the `home` layout.
-  // This will place the `real` view under the
-  // `data-body` directive under the layout.
-  this.render('real', 'home');
-});
-
-/**
- * A global pub/sub that's not associated to a particular route.
- */
-
-server.subscribe('message', function*(){
-  console.log(this.message);
-
-  // Store the message.
-  yield resource('message').insert(this.message);
-
-  // Let all the other users know about the new message.
-  server.emit('message', this.message);
-});
 
 // Listen to the appropriate port:
 app.listen();
