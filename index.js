fb4f98755dd8944e92e3fab846bc95b5ae377dd5
Daniel Fagnan <dnfagnan@gmail.com>
2013-11-08 05:54:30 -0700

    new
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ var tower = require('tower'),
 view.add('template', __dirname + '/views/templates');
 view.add('layout', __dirname + '/views/layouts');
 
+require('tower-memory-adapter');
+
+resource('post')
+  .attr('title')
+  .attr('body', 'text')
+  .attr('published', 'boolean', false);
+
+function create(attrs) {
+  return function(fn){
+    resource('post').init(attrs).save(fn);
+  };
+}
+
 /**
  * GET /
  *
@@ -33,6 +46,15 @@ get('index', '/')
     { username: 'TheHydroImpulse', id: 1 },
     { username: 'Bob', id: 2 }
   ];
+
+  // XXX: Make this work.
+  // this.scope.posts = yield resource('post').all();
+  // this.scope.authors = yield this.scope.posts.get('authors');
+
+  // XXX: Make sessions work.
+  // this.session.id = 123;
+  // this.session.username = 'TheHydroImpulse';
+
   // Render the `index` view, under the `home` layout.
   // This will place the `index` view under the
   // `data-body` directive under the layout.
