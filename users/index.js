8d4df47fd0d41d749bbebfc68650429b80068d35
Daniel Fagnan <dnfagnan@gmail.com>
2013-11-08 06:10:21 -0700

    edit
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- users/index.js
+++ users/index.js
@@ -12,6 +12,20 @@ var route = tower.route;
 route.map(function() {
 
   // Create a new 'users' resource.
+  // This will create a new resource for both the client and
+  // server.
+  //
+  // Currently, the first request always go through the server.
+  // Then, subsequent requests go through the client.
+  //
+  // Example:
+  //
+  //  - visit /users
+  //    = It will render a list of users from the server code.
+  //  - visit / then /users
+  //    = It will render a list of users from the client code.
+  //
+
   this.resource('users');
 
 });
